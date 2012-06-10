29c6e651d1d1e806545beeb18157d90c21d11965
David L. Goldberg <davidlgoldberg@gmail.com>
2012-06-09 21:48:35 -0400

    Removing extra endpoints + refactoring

--- hero_tmpl/views/misc.py
+++ hero_tmpl/views/misc.py
@@ -4,10 +4,6 @@ misc = Blueprint('misc', __name__)
 
 #Routes:
 #====================================
-@misc.route('/')
-def index():
-    return render_template('index.html', content="Home Page", primary_nav="Home")
-
 @misc.route('/about')
 def about():
     return render_template('about.html', primary_nav="About")
